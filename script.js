5da447efe35542ddf024a6a720c1d7dde75ae656
MarceloHcb <marcelohcb@msn.com>
2022-03-29 13:32:23 -0300

    upgrade

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const numbers = document.querySelectorAll("input")
 const btn = document.getElementById("btn")
 const box = document.querySelectorAll(".box")
 const result = document.querySelector(".result")
-const h2 = document.querySelectorAll("h2")
+const num = document.querySelectorAll(".num")
+
 btn.addEventListener("click", number)
 
 let total = numbers.length
-let selected = ""
-numbers.forEach((n, index) => { 
+let selected = ""  
 
+num.forEach((n, index) => { 
   n.addEventListener('click', () => {
     selected = n.id
-
-    h2[index].classList.toggle("active")  })
-  
+     n.classList.toggle("active")
+     console.log(n)
+    })    
+    
 })
+const reload = () =>{
+  window.location.reload()
+}
 function number() {
   box[1].classList.remove("end")
   
@@ -22,6 +27,6 @@ function number() {
     return (result.innerHTML += `<h2> You selected ${selected} out of ${total} </h2>`)
 
   } else {
-    return
+    return (result.innerHTML += `<h2 style="cursor:pointer"onClick ="reload()">Choose rating!</h2>` )
   }
 }
